0f3b736515b799bb7bab1ecfe97550a947f58e2d
Akshay More <moreakshay725@gmail.com>
2026-05-05 13:15:21 +0530

    Implement foreign weather API and WeatherAdaptor class

--- adaptor pattern/index.js
+++ adaptor pattern/index.js
@@ -1,1 +1,16 @@
-console.log("sss");
+const foreignWeatherApi = {
+  fetch_weather(city_name) {
+    return {
+      city_name,
+      temp_fehrenheit: 72,
+      wind_speed_mph: 5,
+      condition: "partly_cloudy",
+    };
+  },
+};
+
+class WeatherAdaptor {
+  constructor(foreignAPI) {
+    this._api = foreignAPI;
+  }
+}
